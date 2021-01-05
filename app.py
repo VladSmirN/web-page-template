743d55cdc1e0a6d36422bf65fc82615d80c5f9e7
Vlad <yavlads2014@yandex.ru>
2021-01-05 18:32:39 +0300

    Лог предсказаний

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from form import PollForm
 from flask import Flask, render_template ,request
-import requests ,json
-
+import requests ,json ,logging,os
+ 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you_sh0uld_r3pl4ce_th1S'
 
+if  os.path.isdir("./log") == False:
+    os.makedirs("./log")
+logging.basicConfig(filename=os.path.join("./log",'prediction.log') , level=logging.INFO)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 @app.route('/')
 def poll():
@@ -19,6 +24,7 @@ def result():
     data.pop('csrf_token')
     try:
         ans = requests.post('http://backend:5000/', json =json.dumps(data))
+        logging.info('Prediction for  '+json.dumps(data)+' - '+ans.text)
         return render_template('result.html', prediction=ans.text)
     except:
          return render_template('fail.html')
