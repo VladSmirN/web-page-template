08bec93261fd3e70d86c3f2ae09046054a435d4a
Vlad <yavlads2014@yandex.ru>
2021-01-05 17:56:24 +0300

    Получение данных из модели + обработка ошибок

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from form import PollForm
-from flask import Flask, render_template
+from flask import Flask, render_template ,request
+import requests ,json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you_sh0uld_r3pl4ce_th1S'
@@ -13,9 +14,14 @@ def poll():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    # Здесь получаем вывод модели и отображаем страничку результата
-    return render_template('result.html', prediction=0)  # Вместо 0 должен быть ответ модели
-
+    data = request.form.to_dict()
+    data.pop('submit')
+    data.pop('csrf_token')
+    try:
+        ans = requests.post('http://backend:5000/', json =json.dumps(data))
+        return render_template('result.html', prediction=ans.text)
+    except:
+         return render_template('fail.html')
 
 if __name__ == '__main__':
     app.run()
